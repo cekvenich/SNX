92e0896a381a33c29898efbfbe663b728290b753
Vic <vic@eml.cc>
2020-01-18 15:05:52 -0500

    wasabi

--- SNXj_90/src/main/java/org/SNXex/db/S3.java
+++ SNXj_90/src/main/java/org/SNXex/db/S3.java
@@ -1,29 +1,25 @@
 package org.SNXex.db;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+
+import org.apache.SNX.util.JACodecUtil;
 
 import io.minio.MinioClient;
 import io.minio.Result;
-import io.minio.errors.MinioException;
-import io.minio.messages.Item;
-
-import org.apache.SNX.util.JUtil;
+import io.minio.messages.Item;;
 
 public class S3 {
 
 	// arn:aws:s3:::snx01
-
+	// wasabiUser	arn:aws:iam::100000048691:user/wasabiUser
+	
 	static MinioClient _mclient;
 
-	static String _bucket = "s3s";
+	static String _bucket = "snx01";
 
 	public static void main(String[] args) throws Throwable {
 
@@ -31,17 +27,16 @@ public class S3 {
 		String access_key = "LUYU20154833CM4L57P8";
 		String secret_key = "oKZmf7b6qmXwydFDqvm7hEGHnYVObZlN9lMtEDLD";
 
-		_mclient = new MinioClient(server, access_key, secret_key);
+		_mclient = new MinioClient(server, access_key, secret_key, "us-east-1");
 
-		String prefix = "Monday/NY"; 
+		String prefix = "Monday/NY";
 		Map row = new HashMap();
 		row.put("a", "A");
 		row.put("b", "B");
 		put(prefix, row);
 		// second row;
 		put(prefix, row);
-		
-		
+
 		System.out.println(find(prefix));
 	}
 
@@ -59,13 +54,13 @@ public class S3 {
 	static protected int getKeyPosition(String prefixPlusKey) {
 		return prefixPlusKey.lastIndexOf('/');
 	}
-	
+
 	/**
 	 * Auto generates they, you only pass the prefix
 	 */
 	static protected void put(String prefix, Map m) throws Throwable {
-		String s = JUtil.toJ(m);
-		InputStream ins = toIns(s);
+		String s = JACodecUtil.toJ(m);
+		InputStream ins = JACodecUtil.toIns(s);
 		String key = "";
 		_mclient.putObject(_bucket, prefix + "/" + key, ins, "application/octet-stream");
 	}
@@ -79,10 +74,9 @@ public class S3 {
 	 */
 	static protected Map getAsMap(String prefixPlusKey) throws Throwable {
 		InputStream ins = _mclient.getObject(_bucket, prefixPlusKey);
-		String s = toStr(ins);
+		String s = JACodecUtil.toStr(ins);
 
-		return JUtil.toMap(s);
+		return JACodecUtil.toMap(s);
 	}
-
 
 }// class
